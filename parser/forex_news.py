a459a3f4776ddcc34f30831ba040ca871d900327
DarkCodeIT <maksplay298@gmail.com>
2024-05-28 20:38:42 +0500

    f

--- parser/forex_news.py
+++ parser/forex_news.py
@@ -7,7 +7,6 @@ headers = {
 }
 
 async def get_data(url: str):
-	data = {}
 
 	async with aiohttp.ClientSession() as session:
 		async with session.get(url=url, headers=headers) as response:
@@ -19,29 +18,44 @@ async def get_data(url: str):
 			#Try find link to video
 			if main.find("div", class_="outerEleWrapper"):
 				video_url = main.find("div", class_="outerEleWrapper").find("source").get("src")
-				print(video_url)
 
-			elif main.find_all("p"):
-				text = main.find_all("p")
+			if main.find_all("p"):
+				code = main.find_all("p")
+				text = ""
 
-				for item in text :
-					print(item.text())
+				for item in code :
+					text += f"{item.text}\n"
+
+			#author
+			author = soup.find("div", class_="flex flex-col").find("div", class_="text-lg font-semibold leading-7 text-[#181C21]").find('a').text
+
+			date = soup.find("div", class_="flex flex-col").find("div", class_="mt-2 flex flex-col gap-2 text-xs md:mt-2.5 md:gap-2.5").text
 			
+			data = {
+				"author" : author,
+				"text" : text,
+				"video_link" : video_url,
+				"link" : url,
+				"date" : date
+			}
+			return data
 
 
-async def middle_pagination(midle_url: str) -> list:
+async def middle_pagination(middle_url: str) -> list:
 	middle_tasks = []
 
 	async with aiohttp.ClientSession() as session:
-		async with session.get(url=midle_url, headers=headers) as response:
+		async with session.get(url=middle_url, headers=headers) as response:
 			
 			soup = bs4.BeautifulSoup(await response.text(), "lxml")
 
 			tag_div = soup.find("div", id="contentSection").find_all("div", class_="textDiv")
-			
+
 			for item in tag_div:
 				link = f"https://ru.investing.com/{item.find("a", class_="title").get("href")}"
 				middle_tasks.append(asyncio.create_task(get_data(url=link)))
+			
+			return middle_tasks
 
 
 async def main_pagination(main_url: str) -> list:
@@ -66,4 +80,8 @@ async def main_pagination(main_url: str) -> list:
 
 	print("Main Pagination complete")
 	return main_tasks
-asyncio.run(get_data(url="https://ru.investing.com/analysis/article-200313191"))
+
+
+run_main = asyncio.run(main_pagination(main_url="https://ru.investing.com/analysis/forex"))
+run_middle = tuple(asyncio.gather(*run_main))
+# # run_get_data = asyncio.gather(*run_middle)
